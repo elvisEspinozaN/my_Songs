37700eda21b12e8f42b212e34071a5b654123dcb
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-02-25 14:34:29 -0500

    wip: inlcude function in urls.py to load main_app

--- mySongs/urls.py
+++ mySongs/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+
+# include function enables to inlcud entire URLs cofig file from main_app
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include funtion - allows us to load the main_app
+    path('', include('main_app.urls')),
 ]
