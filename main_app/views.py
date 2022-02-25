a2f7d6368a8b1e77db17a841fd6994b00961dc9d
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-02-25 16:22:19 -0500

    wip: typo from , -> :

--- main_app/views.py
+++ main_app/views.py
@@ -11,4 +11,4 @@ def about(request):
   return render(request, 'about.html')
 
 def songs_index(request):
-  return render(request, 'songs/index.html', {'songs', songs})
+  return render(request, 'songs/index.html', {'songs': songs})
