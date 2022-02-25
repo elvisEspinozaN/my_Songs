92a1d471aa0c49006d32bb42f8dc18d04d31b25d
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-02-25 15:50:53 -0500

    wip: home html created and rendering on views.py

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse('HelloWorld')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
