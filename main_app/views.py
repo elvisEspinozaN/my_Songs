7e4919ca53ff78e6caa5a78258300d68da2093d1
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-02-25 16:37:02 -0500

    wip: linked database to be passed onto my app

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render
 # this is just http responses into dom
 from django.http import HttpResponse
+from .models import Song
 
 # Create your views here.
 def home(request):
@@ -11,4 +12,5 @@ def about(request):
   return render(request, 'about.html')
 
 def songs_index(request):
+  songs = Song.objects.all()
   return render(request, 'songs/index.html', {'songs': songs})
