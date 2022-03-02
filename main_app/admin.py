be423cd09ed839406e8ea9712cb42fb0dc78f12e
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-02 17:31:12 -0500

    wip: model implementation to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Song, Category
+from .models import Song, Category, Playb
 
 # Register your models here.
 
 admin.site.register(Song)
 admin.site.register(Category)
+admin.site.regirster(Playb)
