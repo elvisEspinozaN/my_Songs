ce1c98010af673f741925e89fe6ecba7c710deca
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-02 16:37:51 -0500

    wip: Play model/human friendly translation/FK connection to models

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+TIMES = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'),
+  ('N', 'Night')
+)
+
 # Create your models here.
 class Category(models.Model):
   name = models.CharField(max_length=50)
@@ -25,3 +31,14 @@ class Song(models.Model):
     # a reverese look up
     # redirect created obj to the show page
     return reverse('detail', kwargs={'song_id': self.id})
+
+class Play(models.Model):
+  date = models.DateField()
+  time = models.CharField(max_length=1, choices=TIMES, default=TIMES[2][1])
+  # common connection through foreign key
+  # CASCADE -> all related instances deleted those w/relation
+  song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # get_< attr name >_display() is available due to attr = choices
+    return f"{self.get_time_display()} on {self.date}"
