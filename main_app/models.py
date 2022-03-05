6897e3a517eeb2249ee7a742fb07652196c32ffc
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-05 09:59:48 -0500

    wip: mtm field, data -> to model to related model

--- main_app/models.py
+++ main_app/models.py
@@ -24,6 +24,7 @@ class Song(models.Model):
   genre = models.CharField(max_length=100)
   artist = models.CharField(max_length=100)
   date = models.IntegerField()
+  categories = models.ManyToManyField(Category)
 
   def __str__(self):
     return self.name
