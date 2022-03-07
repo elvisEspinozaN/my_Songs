1e709f93812791cd7fe5432d7efa9ded45882f0b
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-03-07 16:32:18 -0500

    wip: registered photo model onto admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Song, Category, Playback
+from .models import Song, Category, Playback, Photo
 
 # Register your models here.
 
 admin.site.register(Song)
 admin.site.register(Category)
 admin.site.register(Playback)
+admin.site.register(Photo)
